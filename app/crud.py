85b68705ce5babbd7f081675a9d46c17fd793603
Шипилов Михаил Алексеевич <m.a.shipilov@corp.oooklassika.ru>
2026-03-24 10:33:25 +0700

    first ORM&API commit

---
+++ app/crud.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select, update
+from .models import Base, Route, RouteHistory
+from .scraper import Scraper
+
+load_dotenv()
+DB_USER=os.getenv('DB_USER')
+DB_PASS=os.getenv('DB_PASS')
+DB_HOST=os.getenv('DB_HOST')
+DB_PORT=os.getenv('DB_PORT')
+DB_NAME=os.getenv('DB_NAME')
+
+database_url = f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+engine = create_engine(database_url, echo=True)
+session = Session(engine)
+
+# create tables if not exist
+Base.metadata.create_all(engine)
+
+
+def get_active_routes():
+    stmt = select(Route).where(Route.is_active == True)
+    active_routes = session.execute(stmt).scalars().all()
+    return active_routes
+
+
+def add_route(origin: str, destination: str, abbr: str):
+    stmt = select(Route).where(Route.abbr == abbr)
+    route = session.execute(stmt)
+    # if abbr exists in DB - activate it
+    if route:
+        route.is_active = True
+    # if abbr does not exist in DB - create it
+    else:
+        session.add(Route(origin=origin, destination=destination, abbr=abbr))
+
+    session.commit()
+
+
+def deactivate_route(abbr):
+    session.execute(update(Route).where(Route.abbr==abbr).values(is_active=False))
+    # if isinstance(abbrs, str):
+    #     session.execute(update(Route).where(Route.abbr==abbrs).values(is_active=False))
+    # elif isinstance(abbrs, list):
+    #     session.execute(select(Route).where(Route.abbr.in_(abbrs)).values(is_active=False))
+    # else:
+    #     raise TypeError("abbrs must be an str or a list of strings")
+
+    session.commit()
+    
+
+def extract_data():
+    scraper = Scraper()
+    for route in session.execute(select(Route).where(Route.is_active==True)).first():
+        print(route)
+        price = scraper.scrape(route.abbr)
+        route_history = RouteHistory(
+            route_id=route.id,
+            price=price
+        )
+        session.add(route_history)
+    session.commit()
+
+
+
---
+++ main.py
@@ -0,0 +1,13 @@
+from app import crud
+
+def main():
+    pass
+    # route_list = ['OVBBJS1', 'OVBSHA1', 'OVBCAN1']  # for testing
+    # scraper = Scraper()
+    # price_list = scraper.scrape_list(route_list)
+    # print(price_list)
+
+
+if __name__ == "__main__":
+    print('hello')
+    #main()
